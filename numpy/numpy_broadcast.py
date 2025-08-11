e33c9e370cc562bf83fa73a6a0504c277c072263
vuluongd <vul45845@gmail.com>
2025-08-11 14:50:30 +0700

    thêm numpy_broadcast

---
+++ numpy/numpy_broadcast.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+start_arr = np.array([[0, 0, 0],
+                     [1, 1, 1]]) 
+
+end_arr = np.array([[2, 2, 2],
+                    [3, 3, 3]])
+
+diff = start_arr[:, None, :] - end_arr[None, :, :]
+
+print(diff.shape) 
+print(diff)
