d213f3a39cd038dce2b0789a1e12abc2dc4c023c
vuluongd <vul45845@gmail.com>
2025-10-28 11:13:14 +0700

    them syntax

---
+++ syntax/list_indexing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+list = [
+    np.array([0.0, 1.0, 2.0]),
+    np.array([0.0, 2.0, 3.0]),
+    np.array([5.0, 4.0, 3.0]),
+    np.array([2.0, 2.0, 2.0])
+]
+
+print("number of array:", len(list))
+print("last array:", list[-1])
+
